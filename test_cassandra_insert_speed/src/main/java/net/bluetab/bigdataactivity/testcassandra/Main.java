2fc7103af89f4024a6c642754f8a4ffdea8aa7a2
germanblanco <german.blanco@gmail.com>
2016-05-30 18:19:37 +0200

    First insert working.

--- test_cassandra_insert_speed/src/main/java/net/bluetab/bigdataactivity/testcassandra/Main.java
+++ test_cassandra_insert_speed/src/main/java/net/bluetab/bigdataactivity/testcassandra/Main.java
@@ -1,26 +1,29 @@
 package net.bluetab.bigdataactivity.testcassandra;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SimpleStatement;
 
 public class Main {
 
-   private Cluster cluster;
+   public Cluster cluster;
 
    public void connect(String node) {
       cluster = Cluster.builder()
             .addContactPoint(node).build();
    }
 
-   public void close() {
-      cluster.close();
-   }
-
    public static void main(String[] args) {
       Main client = new Main();
       client.connect("reto3db");
+      Session session = client.cluster.connect("http_request_log");
       for (int i = 0; i < 1; i++) {
-         System.out.println ("Hello World !!! \n");
+         long nowMillis = System.currentTimeMillis();
+         session.execute(new SimpleStatement("INSERT INTO http_request_time " + 
+                                             "(creation_time, host, id,       insertion_time) VALUES (" +
+                                               nowMillis +  ",0," + i + "," + nowMillis + ")"));
       } 
-      client.close();
+      System.out.println ("Finished !!!\n");
+      client.cluster.close();
    }
 }
