3ae4b5382c9017ff099270246493e2b0b1a99b76
germanblanco <german.blanco@gmail.com>
2016-05-31 08:52:13 +0200

    First version that inserts into Cassandra.

--- server_java/build.sh
+++ server_java/build.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-mkdir -p classes && \
-javac -sourcepath src -d classes src/com/happylife/demo/*.java && \
-cd classes && \
-jar -cfe ../files/dummy-web-server.jar com.happylife.demo.Main * && \
-cd .. && \
-sudo docker build -t reto1server .
+mvn package && docker build -t reto3server .
--- server_java/start.sh
+++ server_java/start.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-if [ -z ${THEDB+x} ]; then 
-  sudo docker run --link reto1db -p 80 --name reto1server -d reto1server
-else
-  sudo docker run -e "THEDB=$THEDB" -p 80 --name reto1server -d reto1server
-fi
+
+docker run --link reto3db --name reto3server -d reto3server
