838789acbde01d4d288fb2577dd6e9f4a127a1a2
germanblanco <german.blanco@gmail.com>
2016-06-01 18:08:38 +0200

    Corregir nombre de servidor y query.

--- load/files/start.sh
+++ load/files/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -z ${THESERVER+x} ]; then sed -i.bak s/reto1server/$THESERVER/ /root/.tsung/tsung.xml ; fi
+if [ ! -z ${THESERVER+x} ]; then sed -i.bak s/reto3server/$THESERVER/ /root/.tsung/tsung.xml ; fi
 sed -i.bak s/arrivalrate="500"/arrivalrate="$RATE"/ /root/.tsung/tsung.xml
 sed -i.bak s/to="10"/to="$HITS"/ /root/.tsung/tsung.xml
 
